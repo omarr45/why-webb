0c888657f5506ac8cca45cf96425067123c0ba90
omarr45 <58887202+omarr45@users.noreply.github.com>
2022-09-30 15:18:20 +0200

    updated jw

--- src/pages/Game02.js
+++ src/pages/Game02.js
@@ -3,7 +3,28 @@ import React from 'react';
 const Game02 = () => {
   return (
     <div className='max-w-7xl mx-auto text-center py-24'>
-      <h2>Game02</h2>
+      <h2 className='font-bold text-3xl'>Mirror Alignment</h2>
+      <div className='game-wrapper flex items-center justify-center'>
+        <div className='sliders bg-amber-200'>
+          <div className='slider'>
+            <input
+              type='range'
+              min='1'
+              max='100'
+              value='50'
+              class='slider'
+              id='myRange'
+            />
+          </div>
+        </div>
+        <div className='imgs relative'>
+          <img
+            className='absolute top-0 left-0 h-28'
+            src='https://res.cloudinary.com/omar45/image/upload/h_800,w_800/v1664543541/why-webb/Jupiter-jwst.jpg'
+            alt='Jpyter photographed by JWST'
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/JamesWebb.js
+++ src/pages/JamesWebb.js
@@ -5,8 +5,8 @@ const JamesWebb = () => {
     <div className='text-center w-full max-w-6xl mx-auto'>
       <img
         className='z-10 mx-auto rotate-12 webb-image'
-        src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JWST_spacecraft_model_2.png/732px-JWST_spacecraft_model_2.png'
-        alt=''
+        src='https://res.cloudinary.com/omar45/image/upload/h_800,w_800/v1664539020/why-webb/jwst-landing.png'
+        alt='James Webb Space Telescope'
       />
     </div>
   );
